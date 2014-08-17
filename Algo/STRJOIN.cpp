8ab738e4756f3ccc34b8fe84517f5745a8b05174
Ju Jin BAE <jujinesy@gmail.com>
2014-08-18 00:26:08 +0900

    STRJOIN풀음

--- Algo/STRJOIN.cpp
+++ Algo/STRJOIN.cpp
@@ -4,65 +4,86 @@
 #include <stdlib.h>
 #include <string.h>
 
+int a[100], size;
 
-int len;
-int arr[101];
-
-int compare( const void *arg1, const void *arg2)
+void push(int n)
 {
-	int p1, p2;
-	p1 = *(int*)arg1;
-	p2 = *(int*)arg2;
+	int tmp, v;
 
-	return (p1-p2);
-}
-
-void input()
-{
-	scanf_s("%d", &len);
-	for( int i = 0 ; i < len ; i++)
-		scanf_s( "%d", &arr[i]);
-}
-
-void process()
-{
-	int div = 0;
-	while(1)
-	{
-		div = len/2 + len%2;
-		len = div;
-
-		qsort( (void *)arr, (size_t)(len), sizeof(int), compare);
-		
-		
-		for( int i = 0 ; i < len ; i++ )
-		{
-			
+	a[size] = n;
+	for (v = size; v > 0; v = (v - 1) / 2) {
+		if (a[v] < a[(v - 1) / 2]) {
+			tmp = a[v];
+			a[v] = a[(v - 1) / 2];
+			a[(v - 1) / 2] = tmp;
 		}
-		
-		if( len == 1)
+		else
 			break;
 	}
-}
-void output()
-{
-	printf_s("1\n");
+	size++;
 }
 
-int main(void)
+int pop()
 {
-	int repeat;
-	FILE *tempFile;
+	int rc = a[0], tmp, v;
 
-	freopen_s(&tempFile, "input.txt", "r", stdin);
-	freopen_s(&tempFile, "output.txt", "w", stdout);
+	if (size == 0)
+		return 0;
 
-	scanf_s("%d", &repeat);
-	while(repeat--)
-	{
-		input();
-		process();
-		output();
+	a[0] = a[--size];
+	a[size] = 2147483647;
+	v = 0;
+
+	while (v * 2 + 1 < size) {
+		if (a[v] > a[v * 2 + 1] || a[v] > a[v * 2 + 2]) {
+			if (a[v * 2 + 1] > a[v * 2 + 2]) {
+				tmp = a[v * 2 + 2];
+				a[v * 2 + 2] = a[v];
+				a[v] = tmp;
+				v = v * 2 + 2;
+			}
+			else {
+				tmp = a[v * 2 + 1];
+				a[v * 2 + 1] = a[v];
+				a[v] = tmp;
+				v = v * 2 + 1;
+			}
+		}
+		else
+			break;
+	}
+	return rc;
+}
+
+int main()
+{
+	int c, i, n, result, val;
+
+	scanf("%d", &c);
+
+	while (c--) {
+		scanf("%d", &n);
+		if (n == 1) {
+			scanf("%d", &val);
+			printf("0\n");
+			continue;
+		}
+
+		result = 0;
+		size = 0;
+		for (i = 0; i < n; i++) {
+			scanf("%d", &val);
+			push(val);
+		}
+
+		while (size > 2) {
+			val = pop() + pop();
+			result += val;
+			push(val);
+		}
+
+		result += (pop() + pop());
+		printf("%d\n", result);
 	}
 
 	return 0;
